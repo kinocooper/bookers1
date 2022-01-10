355d488746ee2a119c66a4c143d4bef868d07db3
EC2 Default User <ec2-user@ip-172-31-81-41.ec2.internal>
2022-01-10 21:36:25 +0000

    [add]各ページルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   #get 'top' => 'homes#top'
   root to: 'homes#top'
   get 'books' => 'books#index'
-  get 'books/edit'
-  get 'books/show'
-  get 'homes/top'
+  post 'books/:id' => 'books#create'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
+  patch 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
