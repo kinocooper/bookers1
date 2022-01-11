4afdddc9fd986ec5e9a22c2851396d28723a337a
EC2 Default User <ec2-user@ip-172-31-81-41.ec2.internal>
2022-01-11 01:45:29 +0000

    [add]インデックスページ・投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,31 @@
 class BooksController < ApplicationController
-  def new
-  end
+  #def new
+  #  @newBook = Book.new
+  #end
 
   def index
+    @books = Book.all
+    @newBook = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to show_book_path(book.id)
   end
 
   def edit
   end
 
   def show
+    @book = Book.find(params[:id])
   end
+
+  def destroy
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title,:body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   #get 'top' => 'homes#top'
   root to: 'homes#top'
   get 'books' => 'books#index'
-  post 'books/:id' => 'books#create'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit'
-  patch 'books/:id' => 'books#update'
-  delete 'books/:id' => 'books#destroy'
+  post 'books/:id' => 'books#create'#,as: 'create_book'
+  get 'books/:id' => 'books#show',as: 'show_book'
+  get 'books/:id/edit' => 'books#edit',as: 'edit_book'
+  patch 'books/:id' => 'books#update',as: 'update_book'
+  delete 'books/:id' => 'books#destroy',as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
